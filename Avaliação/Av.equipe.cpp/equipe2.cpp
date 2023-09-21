25106775d309502f440906c2adb3a3b6fedf530e
thaiscslima <thataylima087@gmail.com>
2023-09-21 14:42:24 -0300

    alteradoavaliacaoequipe

--- Avaliação/Av.equipe.cpp/equipe2.cpp
+++ Avaliação/Av.equipe.cpp/equipe2.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 #include <vector>
-#include <string>
-
 struct Cliente
 {
     std::string CPF;
     std::string Nome;
-    std::string DtNascimento;
+    std::string Dt_nascimento;
     std::string CNH;
 };
 
 struct Veiculo
 {
-    std::string Numero;
+    std::string Renavan;
     std::string Placa;
     std::string Data_Hora_Retirada_Prevista;
     std::string Data_Hora_Entrega_Prevista;
     std::string Loja_Retirada;
-    std::string Loja_Entrega;
 };
-
 struct Ocorrencia
 {
     std::string Descricao;
     std::string Data_Hora;
     std::string Numero_Apolice;
 };
-
 struct Locacao
 {
     Cliente cliente;
@@ -37,176 +32,150 @@ struct Locacao
     Ocorrencia ocorrencia;
 };
 
-void IncluirOcorrencia(std::vector<Locacao> &locacoes)
+void IncluirLocacao(std::vector<Locacao> &locacoes)
 {
-    std::string cpfCliente, placaVeiculo;
+    Locacao novaLocacao;
+
     std::cout << "Informe o CPF do Cliente: ";
-    std::cin >> cpfCliente;
+    std::cin >> novaLocacao.cliente.CPF;
+    std::cout << "Informe o Nome do Cliente: ";
+    std::cin.ignore();
+    std::getline(std::cin, novaLocacao.cliente.Nome);
+    std::cout << "Informe a Data de Nascimento do Cliente (DD/MM/AAAA): ";
+    std::cin >> novaLocacao.cliente.Dt_nascimento;
+    std::cout << "Informe a CNH do Cliente: ";
+    std::cin >> novaLocacao.cliente.CNH;
+
+    std::cout << "Informe o Renavan do Veículo: ";
+    std::cin >> novaLocacao.veiculo.Renavan;
     std::cout << "Informe a Placa do Veículo: ";
-    std::cin >> placaVeiculo;
+    std::cin >> novaLocacao.veiculo.Placa;
+    std::cout << "Informe a Data e Hora de Retirada Prevista (DD/MM/AAAA HH:MM): ";
+    std::cin.ignore();
+    std::getline(std::cin, novaLocacao.veiculo.Data_Hora_Retirada_Prevista);
+    std::cout << "Informe a Data e Hora de Entrega Prevista (DD/MM/AAAA HH:MM): ";
+    std::getline(std::cin, novaLocacao.veiculo.Data_Hora_Entrega_Prevista);
+    std::cout << "Informe a Loja de Retirada: ";
+    std::getline(std::cin, novaLocacao.veiculo.Loja_Retirada);
 
-    bool locacaoEncontrada = false;
-    for (Locacao &locacao : locacoes)
-    {
-        if (locacao.cliente.CPF == cpfCliente && locacao.veiculo.Placa == placaVeiculo)
-        {
-            std::cout << "Informe a Descrição da Ocorrência: ";
-            std::cin.ignore();
-            std::getline(std::cin, locacao.ocorrencia.Descricao);
-            std::cout << "Informe a Data e Hora da Ocorrência (DD/MM/AAAA HH:MM): ";
-            std::getline(std::cin, locacao.ocorrencia.Data_Hora);
-            std::cout << "Informe o Número da Apólice: ";
-            std::cin >> locacao.ocorrencia.Numero_Apolice;
+    std::cout << "Informe a Data e Hora de Retirada (DD/MM/AAAA HH:MM): ";
+    std::getline(std::cin, novaLocacao.Data_Hora_Retirada);
+    std::cout << "A Retirada já aconteceu? (S/N): ";
+    char retiradaAconteceu;
+    std::cin >> retiradaAconteceu;
+    novaLocacao.Realizada = (retiradaAconteceu == 'S' || retiradaAconteceu == 's');
 
-            std::cout << "Ocorrência registrada com sucesso!" << std::endl;
-            locacaoEncontrada = true;
-            break;
-        }
-    }
+    locacoes.push_back(novaLocacao);
 
-    if (!locacaoEncontrada)
-    {
-        std::cout << "Locação não encontrada com o CPF e Placa informados." << std::endl;
-    }
+    std::cout << "Locação registrada com sucesso!" << std::endl;
 }
 
-void ExcluirOcorrencia(std::vector<Locacao> &locacoes)
-{
-    std::string cpfCliente, placaVeiculo;
-    std::cout << "Informe o CPF do Cliente: ";
-    std::cin >> cpfCliente;
-    std::cout << "Informe a Placa do Veículo: ";
-    std::cin >> placaVeiculo;
-
-    bool locacaoEncontrada = false;
-    for (Locacao &locacao : locacoes)
-    {
-        if (locacao.cliente.CPF == cpfCliente && locacao.veiculo.Placa == placaVeiculo)
-        {
-            locacao.ocorrencia.Descricao = "";
-            locacao.ocorrencia.Data_Hora = "";
-            locacao.ocorrencia.Numero_Apolice = "";
-
-            std::cout << "Ocorrência excluída com sucesso!" << std::endl;
-            locacaoEncontrada = true;
-            break;
-        }
-    }
-
-    if (!locacaoEncontrada)
-    {
-        std::cout << "Locação não encontrada com o CPF e Placa informados." << std::endl;
-    }
-}
-
-void AlterarOcorrencia(std::vector<Locacao> &locacoes)
-{
-    std::string cpfCliente, placaVeiculo;
-    std::cout << "Informe o CPF do Cliente: ";
-    std::cin >> cpfCliente;
-    std::cout << "Informe a Placa do Veículo: ";
-    std::cin >> placaVeiculo;
-
-    bool locacaoEncontrada = false;
-    for (Locacao &locacao : locacoes)
-    {
-        if (locacao.cliente.CPF == cpfCliente && locacao.veiculo.Placa == placaVeiculo)
-        {
-
-            std::cout << "Descrição atual da Ocorrência: " << locacao.ocorrencia.Descricao << std::endl;
-            std::cout << "Deseja alterar a Descrição? (S/N): ";
-            char alterarDescricao;
-            std::cin >> alterarDescricao;
-            if (alterarDescricao == 'S' || alterarDescricao == 's')
-            {
-                std::cout << "Informe a nova Descrição da Ocorrência: ";
-                std::cin.ignore();
-                std::getline(std::cin, locacao.ocorrencia.Descricao);
-            }
-
-            std::cout << "Data/Hora atual da Ocorrência: " << locacao.ocorrencia.Data_Hora << std::endl;
-            std::cout << "Deseja alterar a Data/Hora? (S/N): ";
-            char alterarDataHora;
-            std::cin >> alterarDataHora;
-            if (alterarDataHora == 'S' || alterarDataHora == 's')
-            {
-                std::cout << "Informe a nova Data e Hora da Ocorrência (DD/MM/AAAA HH:MM): ";
-                std::cin.ignore();
-                std::getline(std::cin, locacao.ocorrencia.Data_Hora);
-            }
-
-            std::cout << "Número da Apólice atual: " << locacao.ocorrencia.Numero_Apolice << std::endl;
-            std::cout << "Deseja alterar o Número da Apólice? (S/N): ";
-            char alterarApolice;
-            std::cin >> alterarApolice;
-            if (alterarApolice == 'S' || alterarApolice == 's')
-            {
-                std::cout << "Informe o novo Número da Apólice: ";
-                std::cin >> locacao.ocorrencia.Numero_Apolice;
-            }
-
-            std::cout << "Ocorrência alterada com sucesso!" << std::endl;
-            locacaoEncontrada = true;
-            break;
-        }
-    }
-
-    if (!locacaoEncontrada)
-        if (!locacaoEncontrada)
-        {
-            std::cout << "Locação não encontrada com o CPF e Placa informados." << std::endl;
-        }
-}
-
-void ListarOcorrenciasPorCliente(const std::vector<Locacao> &locacoes)
+void ExcluirLocacao(std::vector<Locacao> &locacoes)
 {
     std::string cpfCliente;
-    std::cout << "Informe o CPF do Cliente: ";
+    std::cout << "Informe o CPF do Cliente da locação a ser excluída: ";
     std::cin >> cpfCliente;
+    std::vector<Locacao>::iterator it = locacoes.begin();
+    bool encontrada = false;
 
-    bool ocorrenciasEncontradas = false;
-
-    for (const Locacao &locacao : locacoes)
+    while (it != locacoes.end())
     {
-        if (locacao.cliente.CPF == cpfCliente && !locacao.ocorrencia.Descricao.empty())
+        if (it->cliente.CPF == cpfCliente)
         {
-            std::cout << "Descrição da Ocorrência: " << locacao.ocorrencia.Descricao << std::endl;
-            std::cout << "Data/Hora da Ocorrência: " << locacao.ocorrencia.Data_Hora << std::endl;
-            std::cout << "Número da Apólice: " << locacao.ocorrencia.Numero_Apolice << std::endl;
-            std::cout << "-----------------------------" << std::endl;
-            ocorrenciasEncontradas = true;
+            it = locacoes.erase(it);
+            encontrada = true;
+        }
+        else
+        {
+            ++it;
         }
     }
 
-    if (!ocorrenciasEncontradas)
+    if (encontrada)
     {
-        std::cout << "Nenhuma ocorrência encontrada para o CPF informado." << std::endl;
+        std::cout << "Locação(s) excluída(s) com sucesso!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Nenhuma locação encontrada com o CPF informado." << std::endl;
     }
 }
 
-void ListarOcorrenciasPorVeiculo(const std::vector<Locacao> &locacoes)
+void AlterarLocacao(std::vector<Locacao> &locacoes)
 {
-    std::string placaVeiculo;
-    std::cout << "Informe a Placa do Veículo: ";
-    std::cin >> placaVeiculo;
+    std::string cpfCliente;
+    std::cout << "Informe o CPF do Cliente da locação a ser alterada: ";
+    std::cin >> cpfCliente;
 
-    bool ocorrenciasEncontradas = false;
-
-    for (const Locacao &locacao : locacoes)
+    for (Locacao &locacao : locacoes)
     {
-        if (locacao.veiculo.Placa == placaVeiculo && !locacao.ocorrencia.Descricao.empty())
+        if (locacao.cliente.CPF == cpfCliente)
         {
-            std::cout << "Descrição da Ocorrência: " << locacao.ocorrencia.Descricao << std::endl;
-            std::cout << "Data/Hora da Ocorrência: " << locacao.ocorrencia.Data_Hora << std::endl;
-            std::cout << "Número da Apólice: " << locacao.ocorrencia.Numero_Apolice << std::endl;
-            std::cout << "-----------------------------" << std::endl;
-            ocorrenciasEncontradas = true;
+            std::cout << "Informe a nova Data e Hora de Retirada (DD/MM/AAAA HH:MM): ";
+            std::cin.ignore();
+            std::getline(std::cin, locacao.Data_Hora_Retirada);
+            std::cout << "A Retirada já aconteceu? (S/N): ";
+            char retiradaAconteceu;
+            std::cin >> retiradaAconteceu;
+            locacao.Realizada = (retiradaAconteceu == 'S' || retiradaAconteceu == 's');
+
+            std::cout << "Locação alterada com sucesso!" << std::endl;
+            return;
         }
     }
 
-    if (!ocorrenciasEncontradas)
+    std::cout << "Nenhuma locação encontrada com o CPF informado." << std::endl;
+}
+
+void ListarLocacoes(const std::vector<Locacao> &locacoes)
+{
+    if (locacoes.empty())
     {
-        std::cout << "Nenhuma ocorrência encontrada para a Placa do Veículo informada." << std::endl;
+        std::cout << "Nenhuma locação cadastrada." << std::endl;
+        return;
+    }
+
+    std::cout << "Lista de Locações:" << std::endl;
+    for (const Locacao &locacao : locacoes)
+    {
+        std::cout << "Cliente: " << locacao.cliente.Nome << " (CPF: " << locacao.cliente.CPF << ")" << std::endl;
+        std::cout << "Veículo: " << locacao.veiculo.Placa << std::endl;
+        std::cout << "Data e Hora de Retirada: " << locacao.Data_Hora_Retirada << std::endl;
+        std::cout << "Locação Realizada: " << (locacao.Realizada ? "Sim" : "Não") << std::endl;
+        std::cout << "-----------------------------" << std::endl;
+    }
+}
+
+void RegistrarOcorrenciaPorVeiculo(std::vector<Locacao> &locacoes)
+{
+    std::string placaVeiculo;
+    std::cout << "Informe a Placa do Veículo para registrar a ocorrência: ";
+    std::cin >> placaVeiculo;
+    bool ocorrenciaRegistrada = false;
+
+    for (Locacao &locacao : locacoes)
+    {
+        if (locacao.veiculo.Placa == placaVeiculo)
+        {
+            Ocorrencia novaOcorrencia;
+            std::cout << "Informe a Descrição da Ocorrência: ";
+            std::cin.ignore();
+            std::getline(std::cin, novaOcorrencia.Descricao);
+            std::cout << "Informe a Data e Hora da Ocorrência (DD/MM/AAAA HH:MM): ";
+            std::getline(std::cin, novaOcorrencia.Data_Hora);
+            std::cout << "Informe o Número da Apólice: ";
+            std::cin >> novaOcorrencia.Numero_Apolice;
+
+            locacao.ocorrencia = novaOcorrencia;
+            ocorrenciaRegistrada = true;
+
+            std::cout << "Ocorrência registrada com sucesso para o veículo de placa: " << placaVeiculo << std::endl;
+        }
+    }
+
+    if (!ocorrenciaRegistrada)
+    {
+        std::cout << "Nenhum veículo encontrado com a placa informada." << std::endl;
     }
 }
 
@@ -217,64 +186,32 @@ int main()
     int escolha;
     do
     {
-        std::cout << "Menu Principal:" << std::endl;
-        std::cout << "1. Gestão de Clientes" << std::endl;
-        std::cout << "2. Gestão de Veículos" << std::endl;
-        std::cout << "3. Gestão de Locações" << std::endl;
-        std::cout << "4. Gestão de Ocorrências" << std::endl;
-        std::cout << "0. Sair do Programa" << std::endl;
+        std::cout << "Menu de Gestão de Locações:" << std::endl;
+        std::cout << "1. Incluir Locação" << std::endl;
+        std::cout << "2. Excluir uma Locação" << std::endl;
+        std::cout << "3. Alterar Locação" << std::endl;
+        std::cout << "4. Listar todas as Locações" << std::endl;
+        std::cout << "5. Registrar Ocorrência por Veículo" << std::endl;
+        std::cout << "0. Sair" << std::endl;
         std::cout << "Escolha uma opção: ";
         std::cin >> escolha;
 
         switch (escolha)
         {
         case 1:
-
+            IncluirLocacao(locacoes);
             break;
         case 2:
-
+            ExcluirLocacao(locacoes);
             break;
         case 3:
-
+            AlterarLocacao(locacoes);
             break;
         case 4:
-            int escolhaOcorrencia;
-            do
-            {
-                std::cout << "Menu de Gestão de Ocorrências:" << std::endl;
-                std::cout << "1. Incluir Ocorrência" << std::endl;
-                std::cout << "2. Excluir Ocorrência" << std::endl;
-                std::cout << "3. Alterar Ocorrência" << std::endl;
-                std::cout << "4. Listar Ocorrências por Cliente" << std::endl;
-                std::cout << "5. Listar Ocorrências por Veículo" << std::endl;
-                std::cout << "0. Voltar ao Menu Principal" << std::endl;
-                std::cout << "Escolha uma opção: ";
-                std::cin >> escolhaOcorrencia;
-
-                switch (escolhaOcorrencia)
-                {
-                case 1:
-                    IncluirOcorrencia(locacoes);
-                    break;
-                case 2:
-                    ExcluirOcorrencia(locacoes);
-                    break;
-                case 3:
-                    AlterarOcorrencia(locacoes);
-                    break;
-                case 4:
-                    ListarOcorrenciasPorCliente(locacoes);
-                    break;
-                case 5:
-                    ListarOcorrenciasPorVeiculo(locacoes);
-                    break;
-                case 0:
-                    std::cout << "Voltando ao Menu Principal." << std::endl;
-                    break;
-                default:
-                    std::cout << "Opção inválida. Tente novamente." << std::endl;
-                }
-            } while (escolhaOcorrencia != 0);
+            ListarLocacoes(locacoes);
+            break;
+        case 5:
+            RegistrarOcorrenciaPorVeiculo(locacoes);
             break;
         case 0:
             std::cout << "Saindo do programa." << std::endl;
